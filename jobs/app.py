dcf39409b7b9cb7cf452ec864c7e2389465c293e
yoe herrera <yoeherrera@gmail.com>
2023-03-01 22:49:47 -0500

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -23,7 +23,7 @@ def execute_sql():
     return results
 
 @app.teardown_appcontext
-def close_connection():
+def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
