a006b1d250acbb2f97e37f1ed2a1e3e1424c2b03
yoe herrera <yoeherrera@gmail.com>
2023-03-03 15:34:26 -0500

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,16 +1,15 @@
-from flask import Flask, render_template, g
 import sqlite3
+from flask import Flask, render_template, g
 
 PATH = 'db/jobs.sqlite'
 
 app = Flask(__name__)
 
-
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
-    connection.row_factory = sqlite3.Row    
+    connection.row_factory = sqlite3.Row
     return connection
 
 def execute_sql(sql, values=(), commit=False, single=False):
@@ -30,8 +29,6 @@ def close_connection(exception):
     if connection is not None:
         connection.close()
 
-
-
 @app.route('/')
 @app.route('/jobs')
 def jobs():
@@ -42,3 +39,10 @@ def jobs():
 def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
     return render_template('job.html', job=job)
+
+@app.route('/employer/<employer_id>')
+def employer(employer_id):
+    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
+    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
+    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
