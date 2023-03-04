534ccb85a0a9138d6563d566c7f7448fb49ad6c1
yoe <yoe@mmeta4.capital>
2023-03-03 21:41:21 -0500

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,5 +1,6 @@
 import sqlite3
-from flask import Flask, render_template, g
+import datetime
+from flask import Flask, render_template, g, request, redirect, url_for
 
 PATH = 'db/jobs.sqlite'
 
@@ -47,7 +48,17 @@ def employer(employer_id):
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
 
-@app.route('/review/<employer_id>')
+@app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
 def review(employer_id):
-    review = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
-    return render_template('employer.html', review=review)
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+
+        return redirect(url_for('employer', employer_id=employer_id))
+
+    return render_template('review.html', employer_id=employer_id)
